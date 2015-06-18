02c511b301ad721b2e5068e69164d0fc12b498fc
Sam Verschueren <sam.verschueren@gmail.com>
2015-06-18 14:37:16 +0200

    Implemented the script

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-console.log('Hello World');
+'use strict';
 
-console.log(__dirname);
+// module dependencies
+var path = require('path');
+
+// Load the package file
+var pkg = require(path.join(process.env.WERCKER_ROOT, 'package.json'));
+
+console.log(pkg);
+
+// Use the `name` property if provided, or use `PACKAGE_VERSION` as name
+var key = (process.env.WERCKER_PACKAGE_VERSION_NAME || 'PACKAGE_VERSION').toUpperCase(),
+	value = pkg.version || '0.0.1';
+
+// Export the variable
+process.env[key] = value;
 
 console.log(process.env);
-
-console.log(require('/package.json'));
